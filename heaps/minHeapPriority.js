4a24b21c0cd484108631c3de7a430dc0a381bfce
Aworetan Michael <michaelaworetan@gmail.com>
2025-01-07 01:38:40 +0100

    implemented priorityQueue using minHeap

--- heaps/minHeapPriority.js
+++ heaps/minHeapPriority.js
@@ -3,9 +3,17 @@ class PriorityQueue {
     this.heap = [];
   }
 
+  getLeftChildIndex(index) {
+    return 2 * index + 1;
+  }
+
+  getRightChildIndex(index) {
+    return 2 * index + 2;
+  }
+
   insert(priority, value) {
     this.heap.push({ priority, value });
-    this.bubbleUp(this.value.length - 1);
+    this.bubbleUp(this.heap.length - 1);
   }
 
   bubbleUp(index) {
@@ -38,5 +46,43 @@ class PriorityQueue {
     this.heap[0] = this.heap.pop();
 
     this.heapify(0);
+
+    return root;
+  }
+
+  heapify(index) {
+    let smallest = index;
+    const leftChildIndex = this.getLeftChildIndex(index);
+    const rightChildIndex = this.getRightChildIndex(index);
+
+    if (
+      leftChildIndex < this.heap.length &&
+      this.heap[leftChildIndex].priority < this.heap[smallest].priority
+    ) {
+      smallest = leftChildIndex;
+    }
+
+    if (
+      rightChildIndex < this.heap.length &&
+      this.heap[rightChildIndex].priority < this.heap[smallest].priority
+    ) {
+      smallest = rightChildIndex;
+    }
+
+    if (smallest !== index) {
+      this.swap(smallest, index);
+      this.heapify(smallest);
+    }
   }
 }
+
+const pq = new PriorityQueue();
+pq.insert(3, "Task 3");
+pq.insert(1, "Task 1");
+pq.insert(2, "Task 2");
+
+console.log(pq);
+pq.remove();
+
+console.log(pq);
+
